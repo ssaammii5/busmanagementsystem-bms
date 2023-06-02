8a8bd946b4d16a990e81c97a2bd58e4172d66eb7
Sami <47335566+ssaammii5@users.noreply.github.com>
2023-06-02 19:39:17 +0600

    commit

---
+++ adminpanel.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Panel</title>
+
+  <link rel="stylesheet" href="./css/adminpanel.css">
+  <!--Material icon cdn -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp" />
+
+</head>
+
+<body>
+  <div class="container">
+    <aside>
+      <div class="top">
+        <div class="logo">
+          <img src="./asset/logo.png" alt="">
+          <h2>BUS MANAGEMENT SYSTEM</h2>
+        </div>
+        <div class="close" id="close-btn">
+          <span class="material-symbols-sharp">
+            close
+          </span>
+        </div>
+      </div>
+      <div class="sidebar">
+        <a href="#">
+          <span class="material-symbols-sharp">
+            dashboard
+          </span>
+          <h3>Dashboard</h3>
+        </a>
+        <a href="#">
+          <span class="material-symbols-sharp">
+            assignment_ind
+          </span>
+          <h3>Driver</h3>
+        </a>
+        <a href="#">
+          <span class="material-symbols-sharp">
+            dashboard
+          </span>
+          <h3>Dashboard</h3>
+        </a>
+        <a href="#">
+          <span class="material-symbols-sharp">
+            dashboard
+          </span>
+          <h3>Dashboard</h3>
+        </a>
+        <a href="#">
+          <span class="material-symbols-sharp">
+            dashboard
+          </span>
+          <h3>Dashboard</h3>
+        </a>
+        <a href="#">
+          <span class="material-symbols-sharp">
+            dashboard
+          </span>
+          <h3>Dashboard</h3>
+        </a>
+        <a href="#">
+          <span class="material-symbols-sharp">
+            logout
+          </span>
+          <h3>Logout</h3>
+        </a>
+      </div>
+    </aside>
+  </div>
+</body>
+
+</html>
