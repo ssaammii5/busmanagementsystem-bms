22b890eccb366fdb00590dfec238b2e02b8ba2be
Sami <47335566+ssaammii5@users.noreply.github.com>
2023-06-10 22:44:48 +0600

    commit

--- connection.php
+++ connection.php
@@ -2,9 +2,8 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "crud";
+$dbname = "bus_management";
 
-//$conn = mysqli_connect($servername, $username, $password, $dbname);
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
